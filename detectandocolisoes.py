040a77d705d0105767619ef5eca1ece2a763570a
Rafael Amparo <rafa110304@gmail.com>
2023-03-19 21:51:57 -0300

    Create detectandocolisoes.py

---
+++ detectandocolisoes.py
@@ -0,0 +1,8 @@
+#2321 - Detectando Colisões
+
+rect1 = list(map(int, input().split()))
+rect2 = list(map(int, input().split()))
+if (rect2[2] <= rect1[2] and rect2[2] >= rect1[0]) or (rect2[3] <= rect1[3] and rect2[3] >= rect1[1]) or (rect2[0] <= rect1[2] and rect2[0] >= rect1[0]) or (rect2[1] <= rect1[3] and rect2[1] >= rect1[1]):
+    print('1')
+else:
+    print('0')
